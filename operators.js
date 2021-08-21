8b32228501e5b3ff582293338877424f4b8d8acc
mfarzamalam <mfarzamalam@gmail.com>
2021-08-22 04:31:36 +0500

    operators, loops, conditional_statement, functions

---
+++ operators.js
@@ -0,0 +1,14 @@
+let value1 = 3
+let value2 = 5
+
+let result = value1 + value2;
+let result2 = value1 - value2;
+let result3 = value1 / value2;
+let result4 = value1 * value2;
+let result5 = value1 % value2;
+
+console.log("Addition",result);
+console.log("Subtraction",result2);
+console.log("Division",result3);
+console.log("Multiplication",result4);
+console.log("Modulus",result5);
