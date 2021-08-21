1596e853dc44408c77d579720c6e0cb8960bbeba
mfarzamalam <mfarzamalam@gmail.com>
2021-08-22 04:17:15 +0500

    dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+
+let integer = 10;
+let float  = 10.6;
+let string = "this is a string";
+let array = [1,2,3,4,5,6]
+
+
+console.log("This is integer",integer)
+console.log("This is float",float)
+console.log("This is string",string)
